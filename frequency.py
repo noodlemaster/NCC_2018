eebd68175c357b40624557a6cb893af067c6943f
Andy <andyyang@Andys-MacBook-Pro.local>
2018-10-13 13:38:28 +0100

    hahahah

---
+++ frequency.py
@@ -0,0 +1,36 @@
+import re
+
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z']
+
+def frequency(text):
+    freq_character = []
+    # text = extract_text()
+    for j in alphabet:
+        result = re.findall(j, text)
+        freq_character.append(len(result))
+    probability = []
+    PlainText = re.sub(r'\W', '', text)
+    total_cahracter = len(PlainText)
+    for each in freq_character:
+        probability.append(int(each) / total_cahracter)
+    #print(probability)
+    return probability
+
+def frequency_analysis(text):
+    probability = frequency(text)
+    letter_freq = [0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094,
+                   0.00153, 0.0772, 0.04025, 0.02406, 0.06749, 0.07507, 0.01925, 0.0095,
+                   0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.0236, 0.0015, 0.01974, 0.00074]
+    maximum_e = max(probability)
+    maximum_t = sorted(probability)[-2]
+    if abs(maximum_e - 0.12702) <= 0.18:
+        index_e = probability.index(maximum_e)
+    if abs(maximum_t - 0.09056) <= 0.15:
+        index_t = probability.index(maximum_t)
+    return index_e, index_t
+
+if __name__ == '__main__':
+    text = 'XSFJD JMNRF RUDJV LMYFT GWWHP T'
+    #frequency(text)
+    frequency_analysis(text)
