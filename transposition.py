21a3497b59fac161f23d2f199fadd45c27fdc2a2
zoutei <zoutei@outlook.com>
2018-10-13 22:29:02 +0100

    Transposition done

--- transposition.py
+++ transposition.py
@@ -1,5 +1,5 @@
 import itertools
-from checkenglishness import get_all_english_score_in_text
+from checkenglishness import get_all_english_score_in_text, get_english_score
 
 def get_factors(n):
     factors = []
@@ -46,6 +46,9 @@ def return_to_text(table):
 def generate_permutation(num):
     return list(itertools.permutations(range(num)))
 
+def organise_in_order_of_englishness(cvs):
+    pass
+
 def decrypt_transposition(text, combination):
     plaintext = extract_alphabets(text)
     table = groupcharactors(plaintext, len(combination))
@@ -74,7 +77,9 @@ if __name__ == '__main__':
 
     plaintext = extract_alphabets(text)
     factors = get_factors(len(plaintext))
-    max_columns = 10
+
+    max_columns = 7
+
     factors_to_check = []
     for f in factors:
         if f > max_columns:
@@ -82,28 +87,37 @@ if __name__ == '__main__':
         else:
             factors_to_check.append(f)
     factors_to_check.pop(0) #remove one
+    results = []
     for f in factors_to_check:
         table = groupcharactors(plaintext, f)
         for combination in generate_permutation(f):
+            result = []
             arranged_table = swape_colume(table, combination)
             answer = return_to_text(arranged_table)
-            score = get_all_english_score_in_text(answer)
-            outputfile.write(str(score) + ", ")
+            scores = get_all_english_score_in_text(answer)
+            score = get_english_score(answer)
+            result.append(score)
+            result.append(combination)
+            outputfile.write(str(scores) + ", ")
             for c in combination:
                 outputfile.write(str(c) + ", ")
             outputfile.write("\n")
+            results.append(result)
 
-    # list_tables = []
-    # for combination in generate_permutation(f):
-    #     list_tables.append(swape_colume(table, combination))
-    #
-    # for table in list_tables:
-    #     text = return_to_text(table)
-    #     score = get_english_score(text)
-    #     outputfile.write(str(score) + "," + str(combination) + "\n")
-
-
-
-
-
-
+    #Show results
+    numbertop = 3
+    topnumbers = []
+    for i in range(numbertop):
+        biggest = []
+        for result in results:
+            try:
+                if result[0] > biggest[0]:
+                    biggest = result
+            except IndexError:
+                biggest = result
+        topnumbers.append(biggest)
+        results.remove(biggest)
+    print(topnumbers)
+    for result in topnumbers:
+        print(round(result[0], 2), end='')
+        print(decrypt_transposition(text, result[1]))
