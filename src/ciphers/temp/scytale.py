47eb7f35355c5c1b2a0831e38af4bbd761baeba6
Andy <andyyang@Andys-MacBook-Pro.local>
2018-10-23 23:41:53 +0100

    scytale done

---
+++ src/ciphers/temp/scytale.py
@@ -0,0 +1,25 @@
+from src.ciphers.transposition import *
+from src.tools.textksplit import *
+import re
+
+def scytale(text):
+	PlainText = re.sub(r'\W', '', text)
+	factor = get_factors(len(PlainText))
+	result = []
+	for each in factor:
+		l = textksplit(PlainText, each)
+		word_list = []
+		for s in l:
+			string = ''.join(s)
+			word_list.append(string)
+		text = ''.join(word_list)
+		result.append(text)
+	print(result)
+	return result
+
+if __name__ == '__main__':
+	#file = open('../../../questions/example/scytale.txt', 'r')
+	file = open('../../../questions/2017/7b.txt', 'r')
+	text = file.read()
+	file.close()
+	scytale(text)
