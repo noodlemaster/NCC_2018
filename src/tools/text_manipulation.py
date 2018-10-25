25a163602d3c6f42f28102ec31bdf225b4d08cf9
zoutei <zoutei@outlook.com>
2018-10-25 21:57:48 +0100

    Added output to Excel

--- src/tools/text_manipulation.py
+++ src/tools/text_manipulation.py
@@ -7,7 +7,19 @@ def removespace(text):
             textnospace.append(c.lower())
     return textnospace
 
-def textksplit(text, n):
+def groupcharactors(text, n):
+    table = []
+    if len(text)%n != 0:
+        return table
+    row_n = int(len(text)/n)
+    for r in range(row_n):
+        row = []
+        for c in range(n):
+            row.append(text[c*row_n + r])
+        table.append(row)
+    return table
+
+def text_k_split(text, n):
     listofchars = []
     for r in range(n):
         chars = []
@@ -19,15 +31,42 @@ def textksplit(text, n):
         listofchars.append(chars)
     return listofchars
 
+def text_split_in_order(text, n):
+    listofchars = []
+    k = math.ceil(len(text)/n)
+    for i in range(k):
+        chars = []
+        for j in range(n):
+            try:
+                chars.append(text[n*i + j])
+            except IndexError:
+                break
+        listofchars.append(chars)
+    return listofchars
+
 def reverse_text(text):
     l = len(text)
     newstring = ""
     for i in range(l):
-        newstring += text[l-i]
+        newstring += text[l-1-i]
     return newstring
+
+def output_csv(split_text, name):
+    outputfile = open(name + '.csv', 'w')
+    for cs in split_text:
+        for c in cs:
+            outputfile.write(c)
+            outputfile.write(',')
+        outputfile.write('\n')
+    outputfile.close()
+
 
 if __name__ == '__main__':
     text = "XSFJD JMNRF RUDJV LMYFT GWWHP TUDIA HWRMS XXAHJ DNBRH QTOFF NWFGH GLDJJ ATQWH UEQEM DMHRH LMCGL ZAYBT HUWIC "
     text = "apple was great"
     # print(textksplit(removespace(text), 4))
-    print(reverse_text(text))
+    file = open('../../questions/2016/4b.txt', 'r')
+    text = file.read()
+    file.close()
+    #Trasposition
+    output_csv(text_split_in_order(reverse_text(removespace(text)), 5), '2016_4b')
